0fc84a46dbe7c3e592824308480977671fbedb68
cxmax <caixi169@hotmail.com>
2017-07-18 21:47:22 +0800

    1.  add simple usage

--- app/src/main/java/com/cxmax/lifecycler/MainActivity.java
+++ app/src/main/java/com/cxmax/lifecycler/MainActivity.java
@@ -3,12 +3,15 @@ package com.cxmax.lifecycler;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.cxmax.library.LifeCycler;
 import com.cxmax.library.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run(Activity activity, Bundle bundle) {
                         Toast.makeText(MainActivity.this , "hahahha" , Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnActivityDestroyedListener(new Consumer() {
+                    @Override
+                    public void run(Activity activity, Bundle bundle) {
+                        Log.d(TAG, "It have hooked onDestroy() callback");
                     }
                 });
     }
