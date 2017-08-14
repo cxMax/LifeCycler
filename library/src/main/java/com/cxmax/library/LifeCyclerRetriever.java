f95999f35022429f7fd4a7b9dc3715702e326f78
cxmax <caixi169@hotmail.com>
2017-08-15 00:40:05 +0800

    1. support LifeCycler initializition under multi thread

--- library/src/main/java/com/cxmax/library/LifeCyclerRetriever.java
+++ library/src/main/java/com/cxmax/library/LifeCyclerRetriever.java
@@ -3,10 +3,8 @@ package com.cxmax.library;
 import android.app.Activity;
 import android.app.Application;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.util.SparseArray;
 
-import com.cxmax.library.utils.Asserts;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * describe :
@@ -14,35 +12,27 @@ import com.cxmax.library.utils.Asserts;
  * Created by caixi on 17-7-13.
  */
 
-public class LifeCyclerRetriever {
+class LifeCyclerRetriever {
 
-    private static final SparseArray<ILifeCycler> sAppIndex;
+    private static volatile ConcurrentHashMap<Integer , ILifeCycler> sAppIndex;
 
     static {
-        sAppIndex = new SparseArray<>();
+        sAppIndex = new ConcurrentHashMap<>();
     }
 
     private static class SingletonHolder {
-        public final static LifeCyclerRetriever instance = new LifeCyclerRetriever();
+        private final static LifeCyclerRetriever instance = new LifeCyclerRetriever();
     }
 
-    public static LifeCyclerRetriever getInstance() {
+    static LifeCyclerRetriever getInstance() {
         return SingletonHolder.instance;
     }
 
-    public ILifeCycler get(@Nullable Activity activity) {
-        checkAllParamsIsReady(activity);
+    synchronized ILifeCycler get(@NonNull Activity activity) {
         return getLifeCyclerByApplication(activity);
     }
 
-    private void checkAllParamsIsReady(@Nullable Activity activity) {
-        Asserts.requireNonNull(activity);
-        if (!Asserts.isOnMainThread()) {
-            throw new IllegalArgumentException("You should register activity lifecycle callbacks in UI thread");
-        }
-    }
-
-    private ILifeCycler getLifeCyclerByApplication(@NonNull Activity activity) {
+    private synchronized ILifeCycler getLifeCyclerByApplication(@NonNull Activity activity) {
         Application application = activity.getApplication();
         ILifeCycler instance = findLifeCyclerByApplication(application);
         if (instance == null) {
@@ -52,7 +42,7 @@ public class LifeCyclerRetriever {
         return instance;
     }
 
-    private static ILifeCycler findLifeCyclerByApplication(@NonNull Application app){
+    private static synchronized ILifeCycler findLifeCyclerByApplication(@NonNull Application app){
         return sAppIndex.get(app.hashCode());
     }
 
